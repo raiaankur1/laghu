207d6d5dce7c1ea48e749060c144a430a74ff262
Ankur Rai <raiaankur1@gmail.com>
2025-07-25 01:24:37 +0530

    set up project structure and core dependencies

---
+++ src/main/java/com/laghu/LaghuApplication.java
@@ -0,0 +1,13 @@
+package com.laghu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaghuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LaghuApplication.class, args);
+    }
+
+}
